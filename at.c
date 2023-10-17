f7fd6456219acbac94ca958320828fe0ef7e64b6
James-chege <chegejames11.cj@gmail.com>
2023-10-17 23:40:22 +0300

    Simple shell

--- at.c
+++ at.c
@@ -1,15 +1,3 @@
-#include "shell.h"
-
-/**
- * checkInteractiveMode - Check if the shell is running in interactive mode.
- * @info: Pointer to the information structure.
- * Returns: 1 if the shell is in interactive mode, 0 otherwise.
- */
-int checkInteractiveMode(info_t *info)
-{
-	        return (isatty(STDIN_FILENO) && info->readfd <= 2);
-}
-
 /**
  * i_Delimiter - Check if a character is a delimiter.
  * @n: The character to be checked.
@@ -18,13 +6,13 @@ int checkInteractiveMode(info_t *info)
  */
 int i_Delimiter(char n, const char *delimiters)
 {
-	        while (*delimiters)
-			        {
-					                if (*delimiters == n)
-								                {
-											                        return (1);
-														                }
-							                delimiters++;
-									        }
-		        return (0);
+	while (*delimiters)
+	{
+		if (*delimiters == n)
+		{
+			return (1);
+		}
+		delimiters++;
+	}
+	return (0);
 }
